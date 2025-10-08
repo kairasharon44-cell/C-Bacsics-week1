e5169c07624b2f88e7a896ad07af6ff704c866f8
kairasharon44-cell <kairasharon.44@gmail.com>
2025-10-09 01:11:39 +0200

    add project4.c

---
+++ project4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    float fahrenheit, celsius;
+
+    printf("enter temperature in celsius: ");
+    scanf("%f", &celsius);
+    
+    fahrenheit=(celsius*9/5)+32;
+    printf("in fahrenheit %.1f\n", fahrenheit);
+    printf("enter temperature in fahrenheit: ");
+    scanf("%f", &fahrenheit);
+    
+    celsius=(fahrenheit-32)*5/9;
+    printf("in celsius %.1f", celsius);
+
+    return 0;
+}
