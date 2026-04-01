2c383ce95eec94f00704a1dc7cc6fad540fe63d6
kairasharon44-cell <kairasharon.44@gmail.com>
2026-04-01 14:10:41 +0200

    day1: simple calculator

---
+++ project5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    float num1,num2,result;
+    char op;
+    
+
+    printf("enter first number:");
+    scanf("%f",&num1);
+    
+
+    printf("enter  second number:");
+    scanf("%f",&num2);
+    
+    printf("choose operation(+,-,*,/):");
